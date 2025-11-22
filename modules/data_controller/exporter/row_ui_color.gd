74c0d2385c3f8c1c4ae5adac2a585ae6246bb498
Matthew S <cicd@sdggamecollective.com>
2025-11-22 02:13:29 -0600

    Add my color picker by default

--- modules/data_controller/exporter/row_ui_color.gd
+++ modules/data_controller/exporter/row_ui_color.gd
@@ -14,6 +14,13 @@ func is_active() -> bool:
 	return line_edit.has_focus() or color_picker_button.has_focus()
 
 func _ready():
+	# Add palette presets to color picker from Global.COLORS (if available)
+	if Global != null:
+		var picker: ColorPicker = color_picker_button.get_picker()
+		for hex_string in Global.COLORS:
+			var color: Color = Color("#" + str(hex_string))
+			picker.add_preset(color)
+	
 	color_picker_button.color_changed.connect(
 		func(c:Color):
 			line_edit.text = color_picker_button.color.to_html().to_upper()
